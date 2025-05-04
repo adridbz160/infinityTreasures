0be0b9a65be513e5eb447f751c70121d8589893d
adridbz160 <adrivbusiness@gmail.com>
2025-05-04 15:30:33 +0200

    compatibilidad astro/netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import dotenv from 'dotenv';
+import netlify from '@astrojs/netlify/functions';
 
 dotenv.config();
 
 // https://astro.build/config
-export default defineConfig({  });
+export default defineConfig({  
+    output: 'server',
+    adapter: netlify({}),
+});

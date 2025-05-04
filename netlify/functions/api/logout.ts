7c6b24ed2234d81ba94d5a523ff0e0d28884d970
adridbz160 <adrivbusiness@gmail.com>
2025-05-04 21:50:50 +0200

    Cambios en logout

--- netlify/functions/api/logout.ts
+++ netlify/functions/api/logout.ts
@@ -1,26 +1,26 @@
-import { serialize } from 'cookie';
+// import { serialize } from 'cookie';
 
-export async function handler() {
-  const headers = new Headers();
+// export async function handler() {
+//   const headers = new Headers();
 
-  // Eliminar la cookie 'session' para cerrar la sesión
-  headers.append(
-    'Set-Cookie',
-    serialize('session', '', {
-      path: '/',
-      httpOnly: true,
-      maxAge: -1, // La cookie se elimina
-    })
-  );
+//   // Eliminar la cookie 'session' para cerrar la sesión
+//   headers.append(
+//     'Set-Cookie',
+//     serialize('session', '', {
+//       path: '/',
+//       httpOnly: true,
+//       maxAge: -1, // La cookie se elimina
+//     })
+//   );
 
-  // Crear una respuesta que redirige al usuario a la página de acceso
-  const response = new Response(null, {
-    status: 302, // Código de estado para redirección
-    headers: {
-      Location: '/acceso', // Redirigir al usuario a la página de acceso
-      ...Object.fromEntries(headers), // Añadir las cabeceras (como la cookie eliminada)
-    },
-  });
+//   // Crear una respuesta que redirige al usuario a la página de acceso
+//   const response = new Response(null, {
+//     status: 302, // Código de estado para redirección
+//     headers: {
+//       Location: '/acceso', // Redirigir al usuario a la página de acceso
+//       ...Object.fromEntries(headers), // Añadir las cabeceras (como la cookie eliminada)
+//     },
+//   });
 
-  return response;
-}
+//   return response;
+// }

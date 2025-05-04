9221b0f02da1ff0079d03022403cb86bcdea25cd
adridbz160 <adrivbusiness@gmail.com>
2025-05-04 22:05:06 +0200

    Codigo descomentado

--- src/pages/acceso.ts
+++ src/pages/acceso.ts
@@ -1,20 +1,20 @@
-// import { parse } from 'cookie';
-// import type { APIRoute } from 'astro';
+import { parse } from 'cookie';
+import type { APIRoute } from 'astro';
 
-// export const GET: APIRoute = async ({ request, redirect }) => {
-//   const cookies = parse(request.headers.get('cookie') || '');
-//   const session = cookies.session; // Verifica si ya está logueado
+export const GET: APIRoute = async ({ request, redirect }) => {
+  const cookies = parse(request.headers.get('cookie') || '');
+  const session = cookies.session; // Verifica si ya está logueado
 
-//   if (session) {
-//     // Si está logueado, redirigimos al perfil
-//     return redirect('/perfil');
-//   }
+  if (session) {
+    // Si está logueado, redirigimos al perfil
+    return redirect('/perfil');
+  }
 
-//   // Si no está logueado, mostramos el formulario de acceso
-//   return new Response(
-//     await (await fetch(import.meta.env.SITE + '/acceso')).text(), // La página de acceso sigue accesible
-//     {
-//       headers: { 'Content-Type': 'text/html' },
-//     }
-//   );
-// };
+  // Si no está logueado, mostramos el formulario de acceso
+  return new Response(
+    await (await fetch(import.meta.env.SITE + '/acceso')).text(), // La página de acceso sigue accesible
+    {
+      headers: { 'Content-Type': 'text/html' },
+    }
+  );
+};

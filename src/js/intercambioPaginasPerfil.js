4ca97a53d922fa56413a585e98dbd50453b8d8c8
adridbz160 <adrivbusiness@gmail.com>
2025-05-18 18:10:34 +0200

    Redirecciones correctas

--- src/js/intercambioPaginasPerfil.js
+++ src/js/intercambioPaginasPerfil.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("#lateralPerfil a[href^='/']").forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
 
- document.querySelectorAll("#lateralPerfil a[href^='/']").forEach((link) => {
-        link.addEventListener("click", (e) => {
-          e.preventDefault();
+      const idSeccion = link.getAttribute("href").replace("/", "");
 
-          const idSeccion = link.getAttribute("href").replace("/", "");
+      // Ocultar todas las secciones
+      document.querySelectorAll(".seccion").forEach((sec) =>
+        sec.classList.remove("active")
+      );
 
-          document.querySelectorAll(".seccion").forEach((sec) =>
-            sec.classList.add("hidden")
-          );
-
-          const activa = document.getElementById(idSeccion);
-          if (activa) activa.classList.remove("hidden");
-        });
-      });
+      // Mostrar la sección correspondiente
+      const activa = document.getElementById(idSeccion);
+      if (activa) activa.classList.add("active");
+    });
+  });
 });
